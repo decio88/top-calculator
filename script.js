a6cd25d2eb5d6842d476773274e8c468d84003f3
decio88 <decio88@users.noreply.github.com>
2023-01-20 13:19:19 +0100

    Added functions to show calculation result
    Issue with calculations after the first one

--- script.js
+++ script.js
@@ -25,3 +25,50 @@ function operate(operation, x, y) {
     return divide(x, y);
   }
 }
+
+let firstOperator;
+let secondOperator;
+let result;
+let operand;
+let displayValue = document.querySelector("#display");
+
+document.querySelectorAll(".number").forEach((btn) => {
+  btn.addEventListener("click", function () {
+    //call display function with text content
+    showDisplay(btn.textContent);
+  });
+});
+
+document.querySelectorAll(".operator").forEach((btn) => {
+  btn.addEventListener("click", function () {
+    if (firstOperator === undefined) {
+      firstOperator = +displayValue.textContent;
+      operand = btn.textContent;
+      clearDisplay();
+      return;
+    } else {
+      secondOperator = +displayValue.textContent;
+      clearDisplay();
+      result = operate(operand, firstOperator, secondOperator);
+      showDisplay(result);
+      clearDisplay();
+      firstOperator = result;
+      operand = btn.textContent;
+      return;
+    }
+    /* Put the display value in the first operator and store the clicked operand. 
+
+       If the first operator is full, put the number in the second operator, run the operate function,
+       put the result of the operation in the first operator, store the new operator in operator. 
+       
+       Call clear display function*/
+  });
+});
+
+function clearDisplay() {
+  displayValue.textContent = "";
+}
+
+function showDisplay(x) {
+  displayValue.textContent += x;
+}
